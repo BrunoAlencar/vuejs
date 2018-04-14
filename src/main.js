c04209102f0a090c653911e371a0d0372f65c740
Bruno Alencar <joaobrunoalencar@gmail.com>
2018-04-14 14:26:16 -0300

    more kidding

--- src/main.js
+++ src/main.js
@@ -1,11 +1,32 @@
 var vue = new Vue({
     el: "#app",
     data: {
-        message: "Hello dear world..."
+        message: "Hello dear world...",
+        array: [
+            'item 1',
+            'item 2'
+        ]
+    },
+    computed: {
+        array: function (array) {
+            return array
+        }
     },
     methods: {
-        call: function (){
+        call: function () {
             this.message = prompt("Tell me a message!?");
+        },
+        addItem: function () {
+            this.array.push(prompt('New item'))
+        },
+        editItem: function (item, index) {
+            this.array[index] = prompt('Edit item', item)
+            console.log(this.array)
         }
     }
 })
+
+// array: [
+//     'item 1',
+//     'item 2'
+// ]
