cc7b64d26d4f294897a04170b6f61d22be9191bd
Bruno Alencar <joaobrunoalencar@gmail.com>
2018-04-14 21:44:56 -0300

    IIFE and v-if added

--- src/main.js
+++ src/main.js
@@ -1,33 +1,32 @@
-var vue = new Vue({
-    el: "#app",
-    data: {
-        message: "Hello dear world...",
-        array: [
-            'item 1',
-            'item 2'
-        ]
-    },
-    computed: {
-        array: function (array) {
-            return array
+(function () {
+    var vue = new Vue({
+        el: "#app",
+        data: {
+            message: "Hello dear world...",
+            array: [
+                'item 1',
+                'item 2'
+            ]
+        },
+
+        methods: {
+            call: function () {
+                this.message = prompt("Tell me a message!?");
+            },
+            addItem: function () {
+                this.array.push(prompt('New item'))
+            },
+            editItem: function (item, index) {
+                this.array.splice(index, 1, prompt('Edit item', item));
+                console.log(this.array)
+            },
+            deleteItem: function (index) {
+                this.array.splice(index, 1);
+            }
         }
-    },
-    methods: {
-        call: function () {
-            this.message = prompt("Tell me a message!?");
-        },
-        addItem: function () {
-            this.array.push(prompt('New item'))
-        },
-        editItem: function (item, index) {
-            this.array.splice(index, 1, prompt('Edit item', item));
-            console.log(this.array)
-        },
-        deleteItem: function (index) {
-            this.array.splice(index, 1);
-        }
-    }
-})
+    })
+})()
+
 
 // array: [
 //     'item 1',
